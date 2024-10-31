d5ab35bbb7eb5e640ddfba684bd909786e6cf97f
MikiZenebe <mikizenebe10gmail.com>
2024-10-31 08:37:03 +0300

    updated

---
+++ client/src/api/index.ts
@@ -0,0 +1,9 @@
+export const getStudentsUrl = `${
+  import.meta.env.VITE_API_URL
+}/api/students/student`;
+export const addStudentsUrl = `${
+  import.meta.env.VITE_API_URL
+}/api/students/newStudent`;
+export const studentUrl = `${
+  import.meta.env.VITE_API_URL
+}/api/students/students`;
